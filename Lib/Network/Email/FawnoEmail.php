2f41cbe7b68791c9dac717e264573b6856f76e7d
Fernando Herrero <alphp@users.noreply.github.com>
2016-07-22 01:04:01 +0200

    Adding mime type detection in attachments function
    Extends the attachments function adding use of mime_content_type function for automatic detection of mime type

--- Lib/Network/Email/FawnoEmail.php
+++ Lib/Network/Email/FawnoEmail.php
@@ -15,7 +15,7 @@
 					$img = array_unique($img[2]);
 					foreach ($img as $file) if (is_file($file)) {
 						$cid = sha1($file);
-						$images['cid:' . $cid] = array('file' => $file, 'mimetype' => mime_content_type($file), 'contentId' => $cid);
+						$images['cid:' . $cid] = array('file' => $file, 'contentId' => $cid);
 						$files['cid:' . $cid] = $file;
 						$cids['cid:' . $cid] = $cid;
 					}
@@ -25,6 +25,46 @@
 			}
 
 			return $render;
+		}
+
+		public function attachments($attachments = null) {
+			if ($attachments === null) {
+				return $this->_attachments;
+			}
+			$attach = array();
+			foreach ((array)$attachments as $name => $fileInfo) {
+				if (!is_array($fileInfo)) {
+					$fileInfo = array('file' => $fileInfo);
+				}
+				if (!isset($fileInfo['file'])) {
+					if (!isset($fileInfo['data'])) {
+						throw new SocketException(__d('cake_dev', 'No file or data specified.'));
+					}
+					if (is_int($name)) {
+						throw new SocketException(__d('cake_dev', 'No filename specified.'));
+					}
+					$fileInfo['data'] = chunk_split(base64_encode($fileInfo['data']), 76, "\r\n");
+				} else {
+					$fileName = $fileInfo['file'];
+					$fileInfo['file'] = realpath($fileInfo['file']);
+					if ($fileInfo['file'] === false || !file_exists($fileInfo['file'])) {
+						throw new SocketException(__d('cake_dev', 'File not found: "%s"', $fileName));
+					}
+					if (is_int($name)) {
+						$name = basename($fileInfo['file']);
+					}
+				}
+				if (!isset($fileInfo['mimetype'])) {
+					if (function_exists('mime_content_type')) {
+						$fileInfo['mimetype'] = mime_content_type($fileInfo['file']);
+					} else {
+						$fileInfo['mimetype'] = 'application/octet-stream';
+					}
+				}
+				$attach[$name] = $fileInfo;
+			}
+			$this->_attachments = $attach;
+			return $this;
 		}
 	}
 ?>
