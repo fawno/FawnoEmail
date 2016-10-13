a10c754e9828d02c411b1be19b6d910d02ec7319
Fernando Herrero <alphp@users.noreply.github.com>
2016-10-14 01:58:53 +0200

    Add files via upload

---
+++ src/FawnoEmail.php
@@ -0,0 +1,72 @@
+<?php
+	namespace Fawno\Mailer;
+
+	use Cake\Mailer\Email;
+
+	class FawnoEmail extends Email {
+		protected function _renderTemplates ($content) {
+			if (!empty($this->_subject) && empty($this->_viewVars['subject'])) {
+				$this->_viewVars['subject'] = $this->_subject;
+			}
+
+			$rendered = parent::_renderTemplates($content);
+
+			if (!empty($rendered['html'])) {
+				$rendered['html'] = str_replace(array('file:', 'file://', 'cid://'), 'cid:', $rendered['html']);
+				if (preg_match_all('~(["\'])cid:([^\1]+)\1~iU', $rendered['html'], $img)) {
+					$img = array_unique($img[2]);
+					foreach ($img as $file) if (is_file($file)) {
+						$cid = sha1($file);
+						$images['cid:' . $cid] = array('file' => $file, 'contentId' => $cid);
+						$files['cid:' . $cid] = $file;
+						$cids['cid:' . $cid] = $cid;
+					}
+					$this->addAttachments($images);
+					$rendered['html'] = str_replace($files, $cids, $rendered['html']);
+				}
+			}
+
+			return $rendered;
+		}
+
+		public function attachments ($attachments = null) {
+			if ($attachments === null) {
+				return $this->_attachments;
+			}
+			$attach = [];
+			foreach ((array) $attachments as $name => $fileInfo) {
+				if (!is_array($fileInfo)) {
+					$fileInfo = ['file' => $fileInfo];
+				}
+				if (!isset($fileInfo['file'])) {
+					if (!isset($fileInfo['data'])) {
+						throw new InvalidArgumentException('No file or data specified.');
+					}
+					if (is_int($name)) {
+						throw new InvalidArgumentException('No filename specified.');
+					}
+					$fileInfo['data'] = chunk_split(base64_encode($fileInfo['data']), 76, "\r\n");
+				} else {
+					$fileName = $fileInfo['file'];
+					$fileInfo['file'] = realpath($fileInfo['file']);
+					if ($fileInfo['file'] === false || !file_exists($fileInfo['file'])) {
+						throw new InvalidArgumentException(sprintf('File not found: "%s"', $fileName));
+					}
+					if (is_int($name)) {
+						$name = basename($fileInfo['file']);
+					}
+				}
+				if (!isset($fileInfo['mimetype'])) {
+					if (function_exists('mime_content_type')) {
+						$fileInfo['mimetype'] = mime_content_type($fileInfo['file']);
+					} else {
+						$fileInfo['mimetype'] = 'application/octet-stream';
+					}
+				}
+				$attach[$name] = $fileInfo;
+			}
+			$this->_attachments = $attach;
+			return $this;
+		}
+	}
+?>
